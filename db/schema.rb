cd02818277a7838b205a29cfee4555b3fd98f358
AJ Foster <web@aj-foster.com>
2014-05-01 13:24:18 -0400

    Add organization model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501171953) do
+ActiveRecord::Schema.define(version: 20140501172403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20140501171953) do
   add_index "orders", ["creative_id"], name: "index_orders_on_creative_id", using: :btree
   add_index "orders", ["organization_id"], name: "index_orders_on_organization_id", using: :btree
   add_index "orders", ["owner_id"], name: "index_orders_on_owner_id", using: :btree
+
+  create_table "organizations", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
