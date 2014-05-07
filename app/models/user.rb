8dc67ec834077383028c5e7501fa8c9c0407be55
AJ Foster <web@aj-foster.com>
2014-05-06 23:25:35 -0400

    Add user name and setup methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 	devise :database_authenticatable, :registerable,
 		:recoverable, :rememberable, :trackable, :validatable
+	ROLES = %w[Unapproved Basic Creative Admin Superadmin]
 
 
 	has_many :assignments
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
 
 	has_many :orders, inverse_of: :owner, foreign_key: 'owner_id'
 	has_many :claimed_orders, class_name: 'Order', inverse_of: :creative, foreign_key: 'creative_id'
+
+
+	validates :first_name, :last_name, presence: true
+
+
+	def name
+		first_name + " " + last_name
+	end
+
+
+	private
+		def setup_user
+			self.role ||= ROLES[0]
+		end
 end
