7319432c344665e5bc832a8bf2fa5ca915d6f6ab
AJ Foster <web@aj-foster.com>
2014-05-14 00:15:58 -0400

    Add validations (untested) and initial setup (untested) to orders.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,29 @@
 class Order < ActiveRecord::Base
 
+	STATUSES = %w[Unapproved Unclaimed Claimed Started Proofing Revising Complete]
+	TYPES = %w[Graphic Web Video]
+	after_initialize :setup_order
+
 	belongs_to :owner, class_name: 'User'
 	belongs_to :creative, class_name: 'User'
 	belongs_to :organization
+
+
+	validates :name, :due, :description, :needs, presence: true
+	validate :two_weeks_until_deadline
+
+
+	def two_weeks_until_deadline
+		return if can? :manage, Order
+
+		if !due.present? || due > DateTime.now + 2.weeks
+			errors.add :due, "Orders require at least two weeks notice."
+		end
+	end
+
+
+	private
+		def setup_order
+			self.status ||= STATUSES[0]
+		end
 end
