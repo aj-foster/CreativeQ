cba6536e22afcdf995af9dcb57d474ab7421beef
AJ Foster <web@aj-foster.com>
2014-05-16 00:36:47 -0400

    Stop working at midnight.  Test everything in the morning.  Night folks.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,17 @@
 class Organization < ActiveRecord::Base
 
+	before_destroy :unlink_orders
+
+
 	has_many :assignments
 	has_many :users, through: :assignments
 
 	has_many :orders
+
+
+	def unlink_orders
+		@orders = Order.where(organization_id: id).each do |order|
+			order.organization = nil
+		end
+	end
 end
