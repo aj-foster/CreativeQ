79809b821dbfda77e6703360ae4a3a2148d01bbb
AJ Foster <web@aj-foster.com>
2014-07-22 18:27:14 -0400

    Add icons to indicate graphics, web, and video orders.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module CreativeQ
 		# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 		# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 		# config.i18n.default_locale = :de
+
+		# Add Icomoon Fonts to the asset pipeline
+		config.assets.paths << Rails.root.join("app", "assets", "fonts")
 	end
 end
