b742a415567fc73110ddd8360e22e1fbe921b7f5
AJ Foster <web@aj-foster.com>
2015-01-03 20:47:52 -0500

    Add comments controller with simple create/destory actions.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+
+	def create
+		@order = Order.find(params[:order_id])
+
+		unless can? :comment_on, @order
+			return redirect_to orders_path, alert: "You aren't allowed to comment on this order."
+		end
+
+		@comment = Comment.new(comment_params)
+		@comment.user = current_user
+		@comment.order = @order
+
+		if @comment.save
+			redirect_to @order, notice: "Your comment has been added."
+		else
+			redirect_to @order, alert: "Error: Could not add comment."
+		end
+	end
+
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		@order = @comment.order
+		
+		unless can? :destroy, @comment
+			return redirect_to @order, alert: "You aren't allowed to delete that comment."
+		end
+
+		if @comment.destroy
+			redirect_to @order, notice: "Comment removed."
+		else
+			redirect_to @order, alert: "Error: Comment could not be removed."
+		end
+	end
+end
