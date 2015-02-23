d4b234d9190fd459b927b27b3148db1fa373c3f6
AJ Foster <web@aj-foster.com>
2015-02-22 21:00:47 -0500

    Add basic validation tests for orders. Still need a test for due_date validation.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    # Validation Tests
+    
+    def test_name_presence_validation
+        order_no_name = orders(:order_no_name)
+        assert_not order_no_name.save, "Saved order without a name"
+    end
+
+    def test_due_date_presence_validation
+        order_no_due_date = orders(:order_no_due_date)
+        assert_not order_no_due_date.save, "Saved order without a due date"
+    end
+
+    def test_description_presence_validation
+        order_no_description = orders(:order_no_description)
+        assert_not order_no_description.save, "Saved order without a description"
+    end
+
+    def test_needs_presence_validation
+        order_no_needs = orders(:order_no_needs)
+        assert_not order_no_needs.save, "Saved order without needs"
+    end
 end
