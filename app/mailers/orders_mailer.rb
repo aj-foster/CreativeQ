bef3169c0e84565f386f363826fe5c4fe35dd048
AJ Foster <web@aj-foster.com>
2015-05-17 19:54:11 -0400

    Add mail for comment creation.
    Needs to be tested.

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -8,4 +8,21 @@ class OrdersMailer < ActionMailer::Base
 			mail(to: recipients, subject: "[CreativeQ] Order Awaiting Approval: #{@order.name}")
 		end
 	end
+
+  def order_comment_created (order, comment)
+    @order = order
+    @comment = comment
+
+    begin
+      recipients = User.find(@order.subscriptions || [])
+    rescue ActiveRecord::RecordNotFound
+      recipients = User.find(@order.subscriptions.map { |uid| User.exists? uid } || [])
+    ensure
+      emails = recipients.select(&:send_emails?).map(&:email) || []
+    end
+
+    if emails.any?
+      mail(to: emails, subject: "[CreativeQ] New Comment on #{@order.name}")
+    end
+  end
 end
